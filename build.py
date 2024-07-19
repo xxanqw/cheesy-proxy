d3d14d41e5f915c10d39918902ae177b39907dbb
Іван Потієнко <potik217@gmail.com>
2024-07-19 19:10:17 +0300

    v1.0.2

--- build.py
+++ build.py
@@ -1,3 +1,3 @@
 from os import system
 
-system('python -m nuitka --standalone --windows-icon-from-ico=icons/icon.png --include-data-dir=icons=icons --windows-console-mode=disable --enable-plugin=pyqt6  --product-name="cheesy proxy" --product-version=1.0.1.0 --file-description="Very easy wireproxy client" --copyright="©️2024 xxanqw" -o "cheesy proxy" main.py')
+system('python -m nuitka --standalone --windows-icon-from-ico=icons/icon.png --include-data-dir=icons=icons --windows-console-mode=disable --enable-plugin=pyqt6  --product-name="cheesy proxy" --product-version=1.0.2.0 --file-description="Very easy wireproxy client" --copyright="©️2024 xxanqw" -o "cheesy proxy" main.py')
