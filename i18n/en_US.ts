b6bdf8142ad938b08753052525ede19f80bd6c31
xxanqw <potik217@gmail.com>
2024-11-04 14:49:57 +0200

    i18n support

---
+++ i18n/en_US.ts
@@ -0,0 +1,127 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="main.py" line="17"/>
+        <source>Settings</source>
+        <translation type="finished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="26"/>
+        <source>Path to client.conf:</source>
+        <translation type="finished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="27"/>
+        <source>Browse</source>
+        <translation type="finished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="36"/>
+        <source>Save</source>
+        <translation type="finished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="45"/>
+        <source>Choose client.conf</source>
+        <translation type="finished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="48"/>
+        <source>Config files (*.conf)</source>
+        <translation type="finished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="58"/>
+        <source>Error</source>
+        <translation type="finished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="60"/>
+        <source>Invalid path</source>
+        <translation type="finished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="71"/>
+        <source>Success</source>
+        <translation type="finished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="73"/>
+        <source>Settings saved successfully</source>
+        <translation type="finished"></translation>
+    </message>
+</context>
+<context>
+    <name>SystemTrayApp</name>
+    <message>
+        <location filename="main.py" line="84"/>
+        <location filename="main.py" line="162"/>
+        <source>cheesy proxy - Idle</source>
+        <translation type="finished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="100"/>
+        <source>Start</source>
+        <translation type="finished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="105"/>
+        <source>Stop</source>
+        <translation type="finished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="113"/>
+        <source>Autostart</source>
+        <translation type="finished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="118"/>
+        <source>Settings</source>
+        <translation type="finished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="125"/>
+        <source>Exit</source>
+        <translation type="finished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="142"/>
+        <source>cheesy proxy - Running</source>
+        <translation type="finished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="144"/>
+        <location filename="main.py" line="164"/>
+        <source>cheesy proxy</source>
+        <translation type="finished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="145"/>
+        <source>Proxy started successfully</source>
+        <translation type="finished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="149"/>
+        <source>Error starting proxy</source>
+        <translation type="finished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="152"/>
+        <source>Configuration Error</source>
+        <translation type="finished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="154"/>
+        <source>wireproxy.exe or client.conf path is invalid.</source>
+        <translation type="finished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="165"/>
+        <source>Proxy stopped successfully</source>
+        <translation type="finished"></translation>
+    </message>
+</context>
+</TS>
---
+++ i18n/uk_UA.ts
@@ -0,0 +1,127 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk_UA">
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="main.py" line="17"/>
+        <source>Settings</source>
+        <translation>Налаштування</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="26"/>
+        <source>Path to client.conf:</source>
+        <translation>Шлях до client.conf:</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="27"/>
+        <source>Browse</source>
+        <translation>Огляд</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="36"/>
+        <source>Save</source>
+        <translation>Зберегти</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="45"/>
+        <source>Choose client.conf</source>
+        <translation>Виберіть client.conf</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="48"/>
+        <source>Config files (*.conf)</source>
+        <translation>Файли конфігурації (*.conf)</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="58"/>
+        <source>Error</source>
+        <translation>Помилка</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="60"/>
+        <source>Invalid path</source>
+        <translation>Недійсний шлях</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="71"/>
+        <source>Success</source>
+        <translation>Успішно</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="73"/>
+        <source>Settings saved successfully</source>
+        <translation>Налаштування успішно збережені</translation>
+    </message>
+</context>
+<context>
+    <name>SystemTrayApp</name>
+    <message>
+        <location filename="main.py" line="83"/>
+        <location filename="main.py" line="161"/>
+        <source>cheesy proxy - Idle</source>
+        <translation>cheesy proxy - Простоює</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="99"/>
+        <source>Start</source>
+        <translation>Запустити</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="104"/>
+        <source>Stop</source>
+        <translation>Зупинити</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="112"/>
+        <source>Autostart</source>
+        <translation>Автозапуск</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="117"/>
+        <source>Settings</source>
+        <translation>Налаштування</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="124"/>
+        <source>Exit</source>
+        <translation>Вихід</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="141"/>
+        <source>cheesy proxy - Running</source>
+        <translation>cheesy proxy - Працює</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="143"/>
+        <location filename="main.py" line="163"/>
+        <source>cheesy proxy</source>
+        <translation>cheesy proxy</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="144"/>
+        <source>Proxy started successfully</source>
+        <translation>Проксі успішно запущено</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="148"/>
+        <source>Error starting proxy</source>
+        <translation>Помилка запуску проксі</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="151"/>
+        <source>Configuration Error</source>
+        <translation>Помилка конфігурації</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="153"/>
+        <source>wireproxy.exe or client.conf path is invalid.</source>
+        <translation>Неправильний шлях до wireproxy.exe або client.conf.</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="164"/>
+        <source>Proxy stopped successfully</source>
+        <translation>Проксі успішно зупинено</translation>
+    </message>
+</context>
+</TS>
